646ae48881dc62a17cd1cd117a21e820f5e37c16
lerlerchan <lkchan@sc.edu.my>
2021-07-26 10:53:45 +0800

    mocktest1 demo
    demonstration of html, css and JS editing using free template

--- MockTest1/js/templatemo-script.js
+++ MockTest1/js/templatemo-script.js
@@ -1,5 +1,7 @@
 const initBg = (autoplay = true) => {
-    const bgImgsNames = ['diagoona-bg-1.jpg', 'diagoona-bg-2.jpg', 'diagoona-bg-3.jpg'];
+    //using array to keep images name
+   // const bgImgsNames = ['diagoona-bg-1.jpg', 'diagoona-bg-2.jpg', 'diagoona-bg-3.jpg'];
+    const bgImgsNames = ['gls_1.jpg', 'gls_2.jpg', 'gls_3.jpg'];
     const bgImgs = bgImgsNames.map(img => "img/" + img);
 
     $.backstretch(bgImgs, {duration: 4000, fade: 500});
